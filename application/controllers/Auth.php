5c6f6f387550a23c9cdbad997a1697021bacb336
irsyadkharisma <irsyadkh@gmail.com>
2019-11-23 23:43:03 +0700

    init

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,14 +3,91 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+
+    }
 
     public function index()
     {
-        $this->load->view('auth/login');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'required|trim');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Login Page';
+            $this->load->view('templates/header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('templates/footer');
+        } else {
+            $this->_login();
+        }
     }
 
-    public function register()
+    public function _login()
     {
-        $this->load->view('auth/register');
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+        $user = $this->db->get_where('users', ['email' => $email])->row_array();
+
+        // jika user nya ada, $user mengembalikan nilai
+        if ($user) {
+            if ($user['is_active'] == 1) {
+                // cek passwordnya
+                if (password_verify($password, $user['password'])) {
+                    echo "passwordnya bener";
+// user ketemu, passwordnya bener, tinggal di-redirect ke halaman tertentu dengan session
+                    $data = [
+                        'email' => $user['email'],
+                        'role_id' => $user['role_id'],
+                    ];
+                    $this->session->set_userdata($data);
+                    redirect('user');
+                } else {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Either your email or password is incorrect!</div>');
+                    redirect('auth');
+                }
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Either your email or password is incorrect!</div>');
+            }
+        }
+    }
+
+    public function registration()
+    {
+
+        $this->form_validation->set_rules('firstName', 'First Name', 'required|trim');
+        $this->form_validation->set_rules('lastName', 'Last Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[users.email]', ['is_unique' => 'This email has already been taken.']);
+        $this->form_validation->set_rules('password', 'Password', 'required|trim');
+        $this->form_validation->set_rules('password2', 'Password Confirmation', 'required|trim|min_length[3]|matches[password2]', [
+            'matches' => 'Password Does Not Match',
+            'min_length' => 'Password is Too Short',
+
+        ]);
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Cilogin Registration';
+            $this->load->view('templates/header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/footer');
+        } else {
+            echo "berhasil";
+            $data = [
+                'firstName' => htmlspecialchars($this->input->post('firstName', true)),
+                'lastName' => htmlspecialchars($this->input->post('lastName', true)),
+                'email' => $this->input->post('email'),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_created' => time(),
+            ];
+
+            $this->db->insert('users', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Your account has been created. Please Login!</div>');
+            redirect('auth');
+        }
     }
 }
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,24 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>SB Admin 2 - Login</title>
-
-  <!-- Custom fonts for this template-->
-  <link href="<?=base_url();?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-  <!-- Custom styles for this template-->
-  <link href="<?=base_url();?>css/sb-admin-2.min.css" rel="stylesheet">
-
-</head>
 
 <body class="bg-gradient-primary">
 
@@ -39,12 +18,16 @@
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                   </div>
-                  <form class="user">
+
+                  <?=$this->session->flashdata('message');?>
+                  <form class="user"  method="post" action="<?=base_url('auth');?>">
                     <div class="form-group">
-                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                      <input type="email" class="form-control form-control-user" id="email" value="<?=set_value('email');?>" name="email" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                      <?=form_error('email', '<small class="text-danger pl-3">', '</small>');?>
                     </div>
                     <div class="form-group">
-                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                      <input type="password" class="form-control form-control-user" id="password" value="<?=set_value('password');?>" name="password" placeholder="Password">
+                      <?=form_error('password', '<small class="text-danger pl-3">', '</small>');?>
                     </div>
                     <div class="form-group">
                       <div class="custom-control custom-checkbox small">
@@ -52,9 +35,9 @@
                         <label class="custom-control-label" for="customCheck">Remember Me</label>
                       </div>
                     </div>
-                    <a href="index.html" class="btn btn-primary btn-user btn-block">
+                    <button type="submit" class="btn btn-primary btn-user btn-block">
                       Login
-                    </a>
+                    </button>
                     <hr>
                     <a href="index.html" class="btn btn-google btn-user btn-block">
                       <i class="fab fa-google fa-fw"></i> Login with Google
@@ -68,7 +51,7 @@
                     <a class="small" href="forgot-password.html">Forgot Password?</a>
                   </div>
                   <div class="text-center">
-                    <a class="small" href="<?=base_url();?>auth/register">Create an Account!</a>
+                    <a class="small" href="<?=base_url();?>auth/registration">Create an Account!</a>
                   </div>
                 </div>
               </div>
@@ -81,17 +64,3 @@
     </div>
 
   </div>
-
-  <!-- Bootstrap core JavaScript-->
-  <script src="<?=base_url();?>vendor/jquery/jquery.min.js"></script>
-  <script src="<?=base_url();?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Core plugin JavaScript-->
-  <script src="<?=base_url();?>vendor/jquery-easing/jquery.easing.min.js"></script>
-
-  <!-- Custom scripts for all pages-->
-  <script src="<?=base_url();?>js/sb-admin-2.min.js"></script>
-
-</body>
-
-</html>
